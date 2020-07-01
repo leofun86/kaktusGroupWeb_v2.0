5a167955b1b080669b8dc41b57f0a419224325fd
leofun86 <leorecord@hotmail.com>
2020-07-01 07:28:28 -0300

    primer subida

---
+++ old/js/kaktuScript.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#btn1").click(function() {
+    $("#leerMas").slideToggle();
+  });
+  $("#btn2").click(function() {
+    $("#leerMas2").slideToggle();
+  });
+});
